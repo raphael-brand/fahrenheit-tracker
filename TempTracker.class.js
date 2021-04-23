b7f43732d26384690453806892d13c86409f32d6
Raphael Brand <info@raphaelbrand.de>
2021-04-23 14:37:38 +0000

    add sorted methods

--- TempTracker.class.js
+++ TempTracker.class.js
@@ -1,23 +1,38 @@
 var TempTracker = function() { 
   var collection = [];
+  var sortedCollection = [];
   var updatedCollection = [];
 
 
   this.track = function() {
 		console.log('tracking ...');
-   console.log(typeof this);
+    console.log(typeof this);
   }
 
   this.add = function(value) {
-		if(Number(value) === NaN) throw new TypeError('value must be a number');
-		if(collection.length == 0) {
-				collection.push(value);
-				return;
+		if(Number(value) === NaN) {
+				const error = new TypeError('value must be a number');
+       console.error('value must be a number'); 
 		}
+
+		collection.push(value);
+		sortedCollection = [...collection.sort()];
+  }
+
+  this.avg = function() {
 
     collection.forEach((index, value) => {
 
+      
 		}); 
+  }
+
+  this.highest = function() {
+   return sortedCollection[sortedCollection.length - 1];
+  }
+  
+  this.lowest = function() {
+		return sortedCollection[0];
   }
 
 }
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var TempTracker = require('./TempTracker.class.js');
 var TempGenerator = require('./TempGenerator.class.js');
 
 var tracker = new TempTracker();
-tracker.track();
-
+tracker.add(5);
+tracker.add(10);
+//tracker.track();
+console.log(tracker.lowest());
+console.log(tracker.highest());
 var generator = new TempGenerator(10);
 console.log(generator.values());
