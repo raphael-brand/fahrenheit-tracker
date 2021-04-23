0fc8aac4199bc298e787e723e4c6a580dd29b58f
Raphael Brand <info@raphaelbrand.de>
2021-04-23 12:48:13 +0000

    add TempTracker setter method

--- TempTracker.class.js
+++ TempTracker.class.js
@@ -2,12 +2,25 @@ var TempTracker = function() {
   var collection = [];
   var updatedCollection = [];
 
+  this.track = this.track.bind(this);
+  this.add = this.add.bind(this);
+
+
   this.track = function() {
 		console.log('tracking ...');
    console.log(typeof this);
   }
 
-  this.track = this.track.bind(this);
+  this.add = function(value) {
+		if(collection.length == 0) {
+				collection.push(value);
+				return;
+		}
+
+    collection.forEach((index, value) => {
+
+		}); 
+  }
 
 }
 module.exports = TempTracker;
